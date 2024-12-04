6c23766193af39f37cd64ec315ab3b3fe810bd9d
Maryline Renaud <r.maryline90@gmail.com>
2024-12-04 19:30:42 +0100

    add circle projectsCards

--- components/projectCards.vue
+++ components/projectCards.vue
@@ -5,6 +5,7 @@
           <h1 class="text-balance text-4xl font-semibold font-parkinsans tracking-tight text-indigo-500 sm:text-5xl">My Projects</h1>
         </div>
         <div class="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <!-- boucle cards-->
           <article v-for="post in posts" :key="post.id" class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
             <img :src="post.imageUrl" alt="" class="absolute inset-0 -z-10 size-full object-cover">
             <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
@@ -29,7 +30,7 @@
       description:
         'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
       imageUrl:
-        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+        'https://plus.unsplash.com/premium_photo-1720744786849-a7412d24ffbf?q=80&w=3209&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       },
     {
         id: 2,
--- features/projetsSection.vue
+++ features/projetsSection.vue
@@ -3,8 +3,20 @@ import ProjectCards from '../components/projectCards.vue';
 </script>
 
 <template>
+  <!--components projectsCards-->
   <section id="projects" class="scroll-mt-24 h-screen-custom">
+  <!-- Cercle bg image -->
+    <div class="relative">
+        <div
+          class="absolute right-10 top-72 h-[400px] w-[400px] -translate-y-1/2 rounded-full bg-indigo-600/10"
+          aria-hidden="true"
+        />
+        <div
+          class="absolute -bottom-32 left-5 h-[400px] w-[400px] -translate-y-1/4 rounded-full bg-purple-600/10"
+          aria-hidden="true"
+        />
     <ProjectCards/>
+    </div>
   </section>
 </template>
 
